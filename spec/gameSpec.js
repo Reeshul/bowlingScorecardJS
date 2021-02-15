6a9f97efcefe856ab28460b91bcf1e58cae27dc1
Reeshul <reeshul@hotmail.com>
2021-02-15 20:28:28 +0000

    test for gutter game

---
+++ spec/gameSpec.js
@@ -0,0 +1,33 @@
+"use-strict";
+
+describe("score", () => {
+  let game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it("outputs a score of 0 after 20 consecutive rolls of hitting 0 pins", () => {
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    expect(game.score()).toBe(0);
+  });
+});
