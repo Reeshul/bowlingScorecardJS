8ef9b61a7c2d689b1a7829c3282f44d18054762e
Reeshul <reeshul@hotmail.com>
2021-02-15 21:21:33 +0000

    added some tests to guide how the arrays should be structured

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -44,9 +44,22 @@ describe("score", () => {
     expect(game.runningScore()).toBe(6);
   });
 
-  it("the first item in the frames array is a 6 after 2 consecutive rolls of hitting 3 pins", () => {
-    game.roll(3);
-    game.roll(3);
-    expect(game.framesArray[0]).toBe(6)
-  })
+  it("the first item in the frames array is a 6 after rolling a 4 and then a 5", () => {
+    game.roll(4);
+    game.roll(5);
+    expect(game.framesArray).toBe([9]);
+    expect(game.allRollsArray).toBe([[4, 5]]);
+    expect(game.currentRollsArray).toBe([]);
+  });
+  it("the first subarray in the allRollsArray is [4,5] after rolling a 4 and then a 5", () => {
+    game.roll(4);
+    game.roll(5);
+    expect(game.allRollsArray).toBe([[4, 5]]);
+    expect(game.currentRollsArray).toBe([]);
+  });
+  it("the currentRollsArray is empty after rolling a 4 and then a 5", () => {
+    game.roll(4);
+    game.roll(5);
+    expect(game.currentRollsArray).toBe([]);
+  });
 });
--- src/game.js
+++ src/game.js
@@ -1,13 +1,23 @@
 class Game {
   constructor() {
-    this.rollsIndex = [];
+    this.currentRollsArray = [];
+    this.allRollsArray = [];
+    this.framesArray = [];
   }
   roll(pins) {
-    this.rollsIndex.push(pins);
+    this.currentRollsArray.push(pins);
+    // if rolls array length has two numbers in it
+    if (this.currentRollsArray.length === 2) {
+      // push the entire array into the allRollsArray array
+
+      // and then empty the currentRollsArray
+
+      // and then push the sum of the newly pushed array in the allRollsArray array in the frames array
+    }
   };
 
   runningScore() {
-    return this.rollsIndex.reduce(
+    return this.currentRollsArray.reduce(
       (accumulator, currentValue) => accumulator + currentValue
     );
   };
